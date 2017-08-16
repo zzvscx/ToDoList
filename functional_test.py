6e96fbab537daa97a00afabdb15862a63dd45d63
zsw <zsw18049252607@163.com>
2017-08-16 12:19:17 +0800

    start

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        browser = webdriver.Firefox()
+        browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
