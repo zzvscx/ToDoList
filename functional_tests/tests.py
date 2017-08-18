9b39134a3828a0bfef2694664250537110c086fd
zsw <zsw18049252607@163.com>
2017-08-18 14:49:03 +0800

    分开不同list的item

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,25 +1,24 @@
-
-import time
 from django.test import LiveServerTestCase
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import time
 class NewVisitorTest(LiveServerTestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
-        #self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+
+    def test_can_start_a_list_for_one_user(self):
 
         self.browser.get(self.live_server_url)
 
@@ -34,27 +33,36 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
         inputbox.send_keys('Buy peacock feathers')
+
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url,'/lists/.+')
-
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
         self.browser.get(self.live_server_url)
-
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -63,12 +71,12 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy milk')
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
-        self.assertNotEqual(edith_list_url, francis_list_url)
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('buy milk', page_text)
-        self.fail('Finish the test!')
+        self.assertIn('Buy milk', page_text)
--- todolists/urls.py
+++ todolists/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
 from django.conf.urls import include, url
-from django.contrib import admin
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
 ]
+
