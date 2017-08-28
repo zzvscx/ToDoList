a575b40129f9c2c54c990135e1be3eded805bf90
zsw <zsw18049252607@163.com>
2017-08-28 16:50:47 +0800

    删除add_item视图

--- lists/views.py
+++ lists/views.py
@@ -7,16 +7,22 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html',{"error":error})
     return redirect('/lists/%d/' % (list_.id,))
 
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
+
     return render(request, 'list.html', {'list': list_})
